9cfee6831e2c968010109fdde152ea849a257d82
BrazhnikDA <65236303+BrazhnikDA@users.noreply.github.com>
2021-09-24 19:27:56 +0300

    Добавлен СД Стек, добавлена функция перевыделения памяти

---
+++ Algorithms_and_data_structures/Data_Structures/Stack.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_and_data_structures.Data_Structures
+{
+    class Stack<T> : IEnumerable<T>
+    {
+        T[] mass;
+        int count;
+        int size;
+
+        // По умолчанию 10 элементов
+        public Stack(int defaultSize = 10)
+        {
+            size = defaultSize;
+            mass = new T[size];
+            count = 0;
+        }
+
+        public void Push(T val)
+        {
+            if(count >= size)
+            {
+                RelocateMemory();
+            }
+            mass[count] = val;
+            count++;
+        }
+
+        public T Pop()
+        {
+            if (Empty())
+                throw new InvalidOperationException("Stack is empty.");
+            T item = mass[--count];
+            mass[count] = default; // сбрасываем ссылку
+            return item;
+        }
+
+        public T Top()
+        {
+            if (Empty())
+                throw new InvalidOperationException("Stack is empty.");
+            return mass[count - 1];
+        }
+
+        public bool Empty()
+        {
+            return count == 0;
+        }
+
+        // Перевыделение памяти под стек
+        private void RelocateMemory()
+        {
+            T[] tmp = new T[size];
+            tmp = mass;
+            size = size * size;
+            mass = new T[size];
+
+            for(int i = 0; i < tmp.Length; i++)
+                mass[i] = tmp[i];
+        }
+
+        // Реализация интерфейса IEnumerable
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            for(int i = 0; i < count; i++)
+            {
+                yield return mass[i];
+            }
+        }
+    }
+}
--- Algorithms_and_data_structures/Program.cs
+++ Algorithms_and_data_structures/Program.cs
@@ -1,11 +1,12 @@
 ﻿using Algorithms_and_data_structures.Data_Structures.List;
+using Algorithms_and_data_structures.Data_Structures;
 using System;
 
 namespace Algorithms_and_data_structures
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void TestList()
         {
             List<int> list = new List<int>();
             int[] a = { 2, 3, 4, 5, 6, 7 };
@@ -20,7 +21,7 @@ namespace Algorithms_and_data_structures
             Console.WriteLine();
 
             list.AppendFirst(55);
-            
+
             foreach (var val in list)
             {
                 Console.Write(val + " ");
@@ -35,9 +36,34 @@ namespace Algorithms_and_data_structures
                 Console.Write(val + " ");
             }
             Console.WriteLine();
-            
-            Console.WriteLine(list.GetValueIndex(1));
 
+            Console.WriteLine(list.GetValueIndex(1));
+        }
+
+        private static void TestStack()
+        {
+            Stack<int> stack = new Stack<int>(2);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            //stack.Pop();
+            //stack.Pop();
+
+            foreach(var val in stack)
+            {
+                Console.Write(val + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Верхушка - " + stack.Top());
+        }
+
+        static void Main(string[] args)
+        {
+            //TestList();
+              TestStack();
         }
     }
 }
