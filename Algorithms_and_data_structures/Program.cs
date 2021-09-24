18c305030be79f3f9aba3d65751a05c7f8029105
BrazhnikDA <65236303+BrazhnikDA@users.noreply.github.com>
2021-09-24 17:26:40 +0300

    Отчистил ветку

--- Algorithms_and_data_structures/Program.cs
+++ Algorithms_and_data_structures/Program.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using Algorithms_and_data_structures.Sorts;
-
-namespace Algorithms_and_data_structures
+﻿namespace Algorithms_and_data_structures
 {
     class Program
     {
-        private const int size = 5000;
-
         static void Main(string[] args)
         {
 
-            Bubble_Sort.Sort(InputData.CreateData(size));
-            Choise_Sort.Sort(InputData.CreateData(size));
         }
     }
 }
